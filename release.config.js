eae77444793247633c8d8c4befe2c01e6ef385c4
___Sundae <jennymaelbaldoza@iskolarngbayan.pup.edu.ph>
2024-04-19 01:36:53 +0800

    ci: fixed semantic

--- release.config.js
+++ release.config.js
@@ -1,17 +1,28 @@
 const config = {
     branches: ['main'],
-    plugins: [
-        'semantic-release/commit-analyzer',
-        'semantic-release/release-notes-generator',
-        '@semantic-release/npm',
+    // plugins: [
+    //     'semantic-release/commit-analyzer',
+    //     'semantic-release/release-notes-generator',
+    //     '@semantic-release/npm',
+    //     [
+    //         "@semantic-release/git", {
+    //             "assets": ["dist/*.js", "dist/*js.map"],
+    //             "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    //         }
+    //     ],
+    //     '@semantic-release/github'
+    // ]
+    "plugins": [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
         [
-            "@semantic-release/git", {
-                "assets": ["dist/*.js", "dist/*js.map"],
-                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            "@semantic-release/github",
+            {
+                "assets": ["dist/**"]
             }
         ],
-        '@semantic-release/github'
-    ]
+        "@semantic-release/git"
+    ],
 };
 
 module.exports = config;
